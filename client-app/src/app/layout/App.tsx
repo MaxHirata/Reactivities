e7b09fe7c22df73746a668f4709490e941c07bb8
Max Hirata <max.hirata118@gmail.com>
2019-10-08 16:16:39 -0700

    React Hooks

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,42 @@
+import React, {useState, useEffect, Fragment } from 'react';
+//import logo from './logo.svg';
+import axios from 'axios';
+import { Header, Icon, List, Container } from 'semantic-ui-react';
+import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
+
+
+const App = () => {
+
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+        axios.get<IActivity[]>('http://localhost:5000/api/activities')
+      .then((res) => {
+        console.log(res);
+        setActivities(res.data)
+      });
+  }, []);
+
+
+      return (
+    <Fragment>
+    <NavBar />
+    <Container style={{marginTop: '7em'}}>
+        <List>
+          {
+             activities.map((activity) => (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
+            ))
+          }
+        </List>
+    </Container>
+
+
+    </Fragment>
+  );
+
+
+}
+
+export default App;
