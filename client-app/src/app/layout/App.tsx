76f8d7bf4634658ead09e22ba19b35a69324e208
Max Hirata <max.hirata118@gmail.com>
2019-10-17 07:37:01 -0700

    MobX Added

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,99 +1,116 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
+import React, { useState, useEffect, Fragment, SyntheticEvent, useContext } from 'react';
 //import logo from './logo.svg';
 //import axios from "axios";
 //import { Header, Icon, List, Container } from 'semantic-ui-react';
-import { IActivity } from "../models/activity";
-import NavBar from "../../features/nav/NavBar";
-import ActivityDashboard from "../../features/activity/dashboard/ActivityDashboard";
-import { Container } from "semantic-ui-react";
-import agent from "../api/agent";
-import LoadingComponent from "./LoadingComponent";
+import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activity/dashboard/ActivityDashboard';
+import { Container } from 'semantic-ui-react';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
+import ActivityStore from '../stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 const App = () => {
-  const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
-    null
-  );
+	const activityStore = useContext(ActivityStore);
 
-  const [editMode, setEditMode] = useState(false);
+	const [ activities, setActivities ] = useState<IActivity[]>([]);
+	const [ selectedActivity, setSelectedActivity ] = useState<IActivity | null>(null);
 
-  const [loading, setLoading] = useState(true);
+	const [ editMode, setEditMode ] = useState(false);
 
-  const [submitting, setSubmitting] = useState(false);
+	const [ loading, setLoading ] = useState(true);
 
-  const [target, setTarget] = useState('');
+	const [ submitting, setSubmitting ] = useState(false);
 
-  const handleSelectedActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
-    setEditMode(false);
-  };
+	const [ target, setTarget ] = useState('');
 
-  const handleCreateActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false));
-  };
+	const handleSelectedActivity = (id: string) => {
+		setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+		setEditMode(false);
+	};
 
-  const handleEditActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.update(activity).then(() => {
-          setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
-    }).then(() => setSubmitting(false));
+	const handleCreateActivity = (activity: IActivity) => {
+		setSubmitting(true);
+		agent.Activities
+			.create(activity)
+			.then(() => {
+				setActivities([ ...activities, activity ]);
+				setSelectedActivity(activity);
+				setEditMode(false);
+			})
+			.then(() => setSubmitting(false));
+	};
 
-  };
+	const handleEditActivity = (activity: IActivity) => {
+		setSubmitting(true);
+		agent.Activities
+			.update(activity)
+			.then(() => {
+				setActivities([ ...activities.filter((a) => a.id !== activity.id), activity ]);
+				setSelectedActivity(activity);
+				setEditMode(false);
+			})
+			.then(() => setSubmitting(false));
+	};
 
-  const handlesDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>,id: string) => {
-    setSubmitting(true);
-    setTarget(event.currentTarget.name);
-    agent.Activities.delete(id).then( () => {
-      setActivities([...activities.filter(a => a.id !== id)]);
-    }).then(() => setSubmitting(false));
-    
-  };
+	const handlesDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+		setSubmitting(true);
+		setTarget(event.currentTarget.name);
+		agent.Activities
+			.delete(id)
+			.then(() => {
+				setActivities([ ...activities.filter((a) => a.id !== id) ]);
+			})
+			.then(() => setSubmitting(false));
+	};
 
-  const handleOpenCreateForm = () => {
-    setSelectedActivity(null);
-    setEditMode(true);
-  };
+	const handleOpenCreateForm = () => {
+		setSelectedActivity(null);
+		setEditMode(true);
+	};
 
-  useEffect(() => {
-    agent.Activities.list().then(res => {
-      let activities: IActivity[] = [];
-      res.forEach(activity => {
-        activity.date = activity.date.split(".")[0];
-        activities.push(activity);
-      });
-      setActivities(activities);
-    }).then(() => setLoading(false));
-  }, []);
+	useEffect(
+		() => {
+			activityStore.loadActivities();
 
-  if(loading) return <LoadingComponent content='Loading Activities...'/>
+			// agent.Activities
+			// 	.list()
+			// 	.then((res) => {
+			// 		let activities: IActivity[] = [];
+			// 		res.forEach((activity) => {
+			// 			activity.date = activity.date.split('.')[0];
+			// 			activities.push(activity);
+			// 		});
+			// 		setActivities(activities);
+			// 	})
+			// 	.then(() => setLoading(false));
+		},
+		[ activityStore ]
+	);
 
-  return (
-    <Fragment>
-      <NavBar openCreateForm={handleOpenCreateForm} />
-      <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard
-          activities={activities}
-          selectActivity={handleSelectedActivity}
-          selectedActivity={selectedActivity!}
-          editMode={editMode}
-          setEditMode={setEditMode}
-          setSelectedActivity={setSelectedActivity}
-          createActivity={handleCreateActivity}
-          editActivity={handleEditActivity}
-          deleteActivity={handlesDeleteActivity}
-          submitting={submitting}
-          target={target}
-        />
-      </Container>
-    </Fragment>
-  );
+	if (activityStore.loadingInitial) return <LoadingComponent content="Loading Activities..." />;
+
+	return (
+		<Fragment>
+			<NavBar openCreateForm={handleOpenCreateForm} />
+			<Container style={{ marginTop: '7em' }}>
+				<ActivityDashboard
+					activities={activityStore.activities}
+					selectActivity={handleSelectedActivity}
+					selectedActivity={selectedActivity!}
+					editMode={editMode}
+					setEditMode={setEditMode}
+					setSelectedActivity={setSelectedActivity}
+					createActivity={handleCreateActivity}
+					editActivity={handleEditActivity}
+					deleteActivity={handlesDeleteActivity}
+					submitting={submitting}
+					target={target}
+				/>
+			</Container>
+		</Fragment>
+	);
 };
 
-export default App;
+export default observer(App);
--- client-app/src/features/activity/dashboard/ActivityList.tsx
+++ client-app/src/features/activity/dashboard/ActivityList.tsx
@@ -1,74 +1,69 @@
-import React, { SyntheticEvent } from "react";
-import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
+import React, { SyntheticEvent } from 'react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+import { observer } from 'mobx-react-lite';
 
 interface IProps {
-  activities: IActivity[];
-  selectActivity: (id: string) => void;
-  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>,id: string) => void;
-  submitting: boolean;
-  target: string;
+	activities: IActivity[];
+	selectActivity: (id: string) => void;
+	deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+	submitting: boolean;
+	target: string;
 }
 
-const ActivityList: React.FC<IProps> = ({
-  activities,
-  selectActivity,
-  deleteActivity,
-  submitting,
-  target
-}) => {
-  return (
-    <Segment clearing>
-      <Item.Group divided>
-        {activities.map(activity => (
-          <Item key={activity.id}>
-            <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
-              <Item.Meta>{activity.date}</Item.Meta>
-              <Item.Description>
-                <div>{activity.description}</div>
-                <div>
-                  {activity.city}, {activity.venue}
-                </div>
-              </Item.Description>
-              <Item.Extra>
-                <Button
-                  onClick={() => selectActivity(activity.id)}
-                  floated="right"
-                  content="View"
-                  color="blue"
-                />
-                <Button
-                  name={activity.id}
-                  loading={target === activity.id && submitting}
-                  onClick={(e) => deleteActivity(e, activity.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                />
-                <Label basic content={activity.category} />
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-        ))}
+const ActivityList: React.FC<IProps> = ({ activities, selectActivity, deleteActivity, submitting, target }) => {
+	return (
+		<Segment clearing>
+			<Item.Group divided>
+				{activities.map((activity) => (
+					<Item key={activity.id}>
+						<Item.Content>
+							<Item.Header as="a">{activity.title}</Item.Header>
+							<Item.Meta>{activity.date}</Item.Meta>
+							<Item.Description>
+								<div>{activity.description}</div>
+								<div>
+									{activity.city}, {activity.venue}
+								</div>
+							</Item.Description>
+							<Item.Extra>
+								<Button
+									onClick={() => selectActivity(activity.id)}
+									floated="right"
+									content="View"
+									color="blue"
+								/>
+								<Button
+									name={activity.id}
+									loading={target === activity.id && submitting}
+									onClick={(e) => deleteActivity(e, activity.id)}
+									floated="right"
+									content="Delete"
+									color="red"
+								/>
+								<Label basic content={activity.category} />
+							</Item.Extra>
+						</Item.Content>
+					</Item>
+				))}
 
-        <Item>
-          <Item.Content>
-            <Item.Header as="a">Title</Item.Header>
-            <Item.Meta>Date</Item.Meta>
-            <Item.Description>
-              <div>Description</div>
-              <div>City, Venue</div>
-            </Item.Description>
-            <Item.Extra>
-              <Button floated="right" content="View" color="blue" />
-              <Label basic content="Category" />
-            </Item.Extra>
-          </Item.Content>
-        </Item>
-      </Item.Group>
-    </Segment>
-  );
+				<Item>
+					<Item.Content>
+						<Item.Header as="a">Title</Item.Header>
+						<Item.Meta>Date</Item.Meta>
+						<Item.Description>
+							<div>Description</div>
+							<div>City, Venue</div>
+						</Item.Description>
+						<Item.Extra>
+							<Button floated="right" content="View" color="blue" />
+							<Label basic content="Category" />
+						</Item.Extra>
+					</Item.Content>
+				</Item>
+			</Item.Group>
+		</Segment>
+	);
 };
 
-export default ActivityList;
+export default observer(ActivityList);
