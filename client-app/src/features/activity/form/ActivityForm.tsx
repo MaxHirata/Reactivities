b42506d05c64f484331cf35d3f507fa03580c262
Max Hirata <max.hirata118@gmail.com>
2019-11-13 07:20:38 -0800

    currently fixing app for form handlers

--- client-app/src/features/activity/form/ActivityForm.tsx
+++ client-app/src/features/activity/form/ActivityForm.tsx
@@ -2,26 +2,34 @@ import React, { useState, FormEvent, useContext, useEffect } from "react";
 import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
-import ActivityStore from '../../../app//stores/activityStore';
-import {observer} from 'mobx-react-lite';
+import ActivityStore from "../../../app//stores/activityStore";
+import { observer } from "mobx-react-lite";
 import { RouteComponentProps } from "react-router";
-import {Form as FinalForm, Field } from 'react-final-form';
+import { Form as FinalForm, Field } from "react-final-form";
 import { TextInput } from "../../../app/common/form/TextInput";
-import { TextAreaInput } from '../../../app/common/form/TextAreaInput';
-import { SelectInput } from '../../../app/common/form/SelectInput';
+import { TextAreaInput } from "../../../app/common/form/TextAreaInput";
+import { SelectInput } from "../../../app/common/form/SelectInput";
 import { category } from "../../../app/common/options/categoryOptions";
+import { DateInput } from "../../../app/common/form/DateInput";
 
 interface DetailParams {
-  id: string
+  id: string;
 }
 
-const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, history}) => {
-
+const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history
+}) => {
   const activityStore = useContext(ActivityStore);
 
-  const {createActivity, editActivity, submitting, activity: initialFormState, loadActivity, clearActivity } = activityStore;
-
-
+  const {
+    createActivity,
+    editActivity,
+    submitting,
+    activity: initialFormState,
+    loadActivity,
+    clearActivity
+  } = activityStore;
 
   // const initializeForm = () => {
   //   if (initialFormState) {
@@ -40,15 +48,14 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, histo
   // };
 
   const [activity, setActivity] = useState<IActivity>({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: '',
-    city: '',
-    venue: ''
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: null,
+    city: "",
+    venue: ""
   });
-
 
   useEffect(() => {
     if (match.params.id && activity.id.length === 0) {
@@ -57,11 +64,17 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, histo
       );
     }
 
-    return () => { clearActivity() }
+    return () => {
+      clearActivity();
+    };
+  }, [
+    loadActivity,
+    clearActivity,
+    match.params.id,
+    initialFormState,
+    activity.id.length
+  ]);
 
-  }, [loadActivity, clearActivity, match.params.id, initialFormState, activity.id.length]);
-
-  
   // const handleSubmit = () => {
   //   if (activity.id.length === 0) {
   //     let newActivity = {
@@ -76,7 +89,7 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, histo
 
   const handleFinalFormSubmit = (values: any) => {
     console.log(values);
-  }
+  };
 
   // const handleInputChange = (
   //   event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -89,64 +102,67 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, histo
     <Grid>
       <Grid.Column width={10}>
         <Segment clearing>
-        <FinalForm
-          onSubmit={handleFinalFormSubmit}
-          render={({ handleSubmit }) => (
+          <FinalForm
+            onSubmit={handleFinalFormSubmit}
+            render={({ handleSubmit }) => (
               <Form onSubmit={handleSubmit}>
                 <Field
                   name="title"
                   placeholder="Title"
                   value={activity.title}
-                  component= {TextInput}
+                  component={TextInput}
                 />
                 <Field
-                  component= {TextAreaInput}
+                  component={TextAreaInput}
                   name="description"
                   rows={3}
                   placeholder="Description"
                   value={activity.description}
                 />
                 <Field
-                  component= {SelectInput}
+                  component={SelectInput}
                   options={category}
                   name="category"
                   placeholder="Category"
                   value={activity.category}
                 />
                 <Field
-                  component= {TextInput}
+                  component={DateInput}
                   name="date"
-                  type="datetime-local"
                   placeholder="Date"
-                  value={activity.date}
+                  value={activity.date!}
                 />
                 <Field
-                  component= {TextInput}
+                  component={TextInput}
                   name="city"
                   placeholder="City"
                   value={activity.city}
                 />
                 <Field
-                  component= {TextInput}
+                  component={TextInput}
                   name="venue"
                   placeholder="Venue"
                   value={activity.venue}
                 />
-                <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
                 <Button
-                  onClick={() => history.push('/activities')}
+                  loading={submitting}
+                  floated="right"
+                  positive
+                  type="submit"
+                  content="Submit"
+                />
+                <Button
+                  onClick={() => history.push("/activities")}
                   floated="right"
                   type="button"
                   content="Cancel"
                 />
-            </Form>
-          )}
-        />
-
+              </Form>
+            )}
+          />
         </Segment>
       </Grid.Column>
     </Grid>
- 
   );
 };
 
