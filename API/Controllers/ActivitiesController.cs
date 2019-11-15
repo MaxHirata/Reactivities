174359ac94c786bc3b49eab9a0d2af079f75f4c4
Max Hirata <max.hirata118@gmail.com>
2019-11-14 16:23:25 -0800

    adding interface for jwt

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,26 +8,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ActivitiesController : ControllerBase
+    public class ActivitiesController : BaseController
     {
-        private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            this._mediator = mediator;
-        }
+        //private readonly IMediator _mediator;
+        // public ActivitiesController(IMediator mediator)
+        // {
+        //     this._mediator = mediator;
+        // }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> List()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query{Id = id});
+            return await Mediator.Send(new Details.Query{Id = id});
         }
 
         [HttpPost]
@@ -35,20 +33,20 @@ namespace API.Controllers
         {
             //if(!ModelState.IsValid) //Used this before there was ApiController Attribute
                 //return BadRequest(ModelState);
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
             command.Id = id;
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            return await _mediator.Send(new Delete.Command{Id = id});
+            return await Mediator.Send(new Delete.Command{Id = id});
         }
 
     }
